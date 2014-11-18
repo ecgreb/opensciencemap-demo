08e1395b13900354191e389a7edfa06a0d7af985
Chuck Greb <charles.greb@gmail.com>
2014-11-17 22:51:36 -0800

    Adds map

--- app/src/main/java/com/ecgreb/demo/opensciencemapdemo/OpenScienceMapActivity.java
+++ app/src/main/java/com/ecgreb/demo/opensciencemapdemo/OpenScienceMapActivity.java
@@ -1,19 +1,31 @@
 package com.ecgreb.demo.opensciencemapdemo;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.oscim.android.MapActivity;
+import org.oscim.backend.AssetAdapter;
+import org.oscim.layers.tile.buildings.BuildingLayer;
+import org.oscim.layers.tile.vector.VectorTileLayer;
+import org.oscim.layers.tile.vector.labeling.LabelLayer;
+import org.oscim.theme.ThemeFile;
+import org.oscim.tiling.source.oscimap4.OSciMap4TileSource;
 
-public class OpenScienceMapActivity extends Activity {
+import java.io.InputStream;
+
+public class OpenScienceMapActivity extends MapActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_science_map);
-    }
 
+        VectorTileLayer baseLayer = map().setBaseMap(new OSciMap4TileSource());
+        map().layers().add(new BuildingLayer(map(), baseLayer));
+        map().layers().add(new LabelLayer(map(), baseLayer));
+        map().setTheme(Theme.DEFAULT);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -21,6 +33,7 @@ public class OpenScienceMapActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_open_science_map, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -35,5 +48,23 @@ public class OpenScienceMapActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public enum Theme implements ThemeFile {
+        DEFAULT("styles/default.xml"),
+        TRONRENDER("styles/tronrender.xml"),
+        NEWTRON("styles/newtron.xml"),
+        OSMARENDER("styles/osmarender.xml");
+
+        private final String mPath;
+
+        private Theme(String path) {
+            mPath = path;
+        }
+
+        @Override
+        public InputStream getRenderThemeAsStream() {
+            return AssetAdapter.readFileAsStream(mPath);
+        }
     }
 }
